12e33ea1fcecb7a9fa1014faf6899acfaee727d6
elizeuangelo <56921633+elizeuangelo@users.noreply.github.com>
2020-07-23 17:19:36 -0300

    Add files via upload

--- token-info-icons.js
+++ token-info-icons.js
@@ -2,15 +2,11 @@ class TokenInfoIcons {
 
 	static async addSpeedButton(app, html, data) {
         
-        let actor = game.actors.get(data.actorId);
-        if (actor === undefined)
-            return;
+        let actor = canvas.tokens.get(data._id).actor;
+        //let actor = game.actors.get(data.actorId);
+        if (actor === undefined) return;
         
-        try {
-            let speed = game.world.system === "pf1" ? data.actorData.data.attributes.speed.land.total : data.actorData.data.attributes.speed.value;
-        } catch {
-            let speed = game.world.system === "pf1" ? actor.data.data.attributes.speed.land.total : actor.data.data.attributes.speed.value;
-        }
+        let speed = game.world.system === "pf1" ? actor.data.data.attributes.speed.land.total : actor.data.data.attributes.speed.value;
         let cleanspeed = speed;
 
         try {
@@ -32,29 +28,18 @@ class TokenInfoIcons {
 
     static async addPerceptionButton(app, html, data) {
         
-        let actor = game.actors.get(data.actorId);
+        let actor = canvas.tokens.get(data._id).actor;
+        //let actor = game.actors.get(data.actorId);
         if (actor === undefined) return;
+        
         let perception = 10;
-
         if (game.world.system === "pf1") {
             perception = actor.data.data.skills.per.mod
         } else if (game.world.system === "pf2e") {
             const proficiency = actor.data.data.attributes.perception.rank ? actor.data.data.attributes.perception.rank * 2 + actor.data.data.details.level.value : 0;
             perception = perception + actor.data.data.abilities[actor.data.data.attributes.perception.ability].mod + proficiency + actor.data.data.attributes.perception.item;
         } else {
-            const flags = data.actorData.flags.dnd5e || actor.data.flags.dnd5e || {};
-            const observant_feat = flags.observantFeat || false;
-            let wis_mod = 0;
-            try {
-                wis_mod = data.actorData.data.abilities.wis.value
-            } catch {
-                wis_mod = actor.data.data.abilities.wis.mod
-            }
-            try {
-                perception = 10 + actor.data.data.attributes.prof * data.actorData.data.skills.prc.value + wis_mod + observant_feat*5;
-            } catch {
-                perception = actor.data.data.skills.prc.passive;
-            }
+            perception = actor.data.data.skills.prc.passive;
         }
         
         let newdiv = '<div class="token-info-container">';
@@ -68,19 +53,15 @@ class TokenInfoIcons {
 
     static async addACButton(app, html, data) {
         
-        let actor = game.actors.get(data.actorId);
-        if (actor === undefined)
-            return;
+        let actor = canvas.tokens.get(data._id).actor;
+        //let actor = game.actors.get(data.actorId);
+        if (actor === undefined) return;
 
         let ac = 10
         if (game.world.system === "pf1") {
           ac = actor.data.data.attributes.ac.normal.total
         } else {
-            try {
-                ac = (isNaN(parseInt(data.actorData.data.attributes.ac.value)) || parseInt(data.actorData.data.attributes.ac.value) === 0) ? 10 : parseInt(data.actorData.data.attributes.ac.value);
-            } catch {
-                ac = (isNaN(parseInt(actor.data.data.attributes.ac.value)) || parseInt(actor.data.data.attributes.ac.value) === 0) ? 10 : parseInt(actor.data.data.attributes.ac.value);
-            }
+          ac = (isNaN(parseInt(actor.data.data.attributes.ac.value)) || parseInt(actor.data.data.attributes.ac.value) === 0) ? 10 : parseInt(actor.data.data.attributes.ac.value);
         }
 
         let newdiv = '<div class="token-info-container">';
