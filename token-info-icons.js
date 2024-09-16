88ea7a9e228d8bf2b6702b92ab8c64250a182d7a
Jan Ole Peek <jopeek@gmail.com>
2024-09-16 11:31:43 -0700

    Update token-info-icons.js

--- token-info-icons.js
+++ token-info-icons.js
@@ -28,7 +28,7 @@ class TokenInfoIcons {
         perceptionTitle = "Perception DC";
       }
        else {
-          perception = actor.data.data.skills?.prc?.passive || 0;
+          perception = actor.system.skills?.prc?.passive || 0;
       }
 
       //console.log("TokenInfoIcons", actor);
